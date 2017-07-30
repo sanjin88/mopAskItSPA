28111465bbb676e569580ae4679443e7b9aa8c70
Sanjin Kapetanović <sanjin.kapetanovich@gmail.com>
2017-07-30 13:49:06 +0200

    Login registration

--- src/components/registration.js
+++ src/components/registration.js
@@ -1,61 +1,62 @@
 import React, { Component } from 'react';
-import '../styles/login.css';
-
-class Login extends Component {
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
+import '../styles/registration.css';
+class Registration extends Component {
     constructor(props) {
         super(props);
-
         this.state = {
             fieldValues: {
+                firstName: '',
+                lastName: '',
                 email: '',
                 password: ''
             },
             fieldErrors: {
+                firstName: '',
+                lastName: '',
                 email: '',
                 password: ''
             }
-
         }
     }
-
     handleChange(e) {
         this.setState({
             fieldValues: Object.assign({}, this.state.fieldValues, { [e.target.name]: e.target.value })
         })
     }
-
     handleSubmit(event) {
         event.preventDefault();
-
         let values = Object.keys(this.state.fieldValues);
-
-
         let errors = {};
         values.forEach((key) => {
             if (!this.state.fieldValues[key]) {
                 errors[key] = "Required field";
             }
         });
-
         if (this.state.fieldValues.password && this.state.fieldValues.password.length < 3) {
             errors.password = "Your password should be minimum 5 characters";
         }
-
-
         this.setState({
             fieldErrors: errors
         });
-        
-        // this.props.actions.loginUser(this.state.fieldValues)
-
     }
-
     render() {
         return (
-            <div className="login">
-                <h2>Login:</h2>
-                <div className="login-fields">
+            <div className="registration">
+                <h2>Registration:</h2>
+                <div className="registration-fields">
                     <form onSubmit={(e) => this.handleSubmit(e)}>
+                        <div className="field-group">
+                            <label htmlFor="firstName">First Name</label>
+                            <input type="text" name="firstName" value={this.state.firstName} onChange={(e) => this.handleChange(e)} />
+                            {this.state.fieldErrors['firstName'] ? this.state.fieldErrors['firstName'] : ''}
+                        </div>
+                        <div className="field-group">
+                            <label htmlFor="lastName">Last Name</label>
+                            <input type="text" name="lastName" value={this.state.lastName} onChange={(e) => this.handleChange(e)} />
+                            {this.state.fieldErrors['lastName'] ? this.state.fieldErrors['lastName'] : ''}
+                        </div>
                         <div className="field-group">
                             <label htmlFor="email">Email</label>
                             <input type="text" name="email" value={this.state.email} onChange={(e) => this.handleChange(e)} />
@@ -71,11 +72,8 @@ class Login extends Component {
                         </div>
                     </form>
                 </div>
-
             </div>
         );
     }
 }
-
-
-export default Login;
+export default Registration;
--- src/routes.js
+++ src/routes.js
@@ -2,13 +2,18 @@ import React from 'react';
 import { Router, Route, IndexRoute } from 'react-router';
 
 import App from './App';
+import Login from './components/login';
+import Registration from './components/registration';
 
 
 const Routes = (props) => {
 
     return (
         <Router {...props}>
-            <Route path="/" component={App} />
+            <Route path="/" component={App}>
+                <Route path="/login" component={Login} />
+                <Route path="/registration" component={Registration} />
+            </Route>
         </Router>
     )
 };
